7a580ec0ef505d27c32a222acfd1049ce5de38cd
Melvin Joseph Mani <melvinjmani@gmail.com>
2024-08-21 22:30:14 +0530

    Production site update

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // Set the current year in the footer
     document.getElementById("currentYear").textContent = new Date().getFullYear();
     // Get the current page slug from the URL
-    const currentPath = window.location.pathname;
-    let currentSlug = getPathAfterHelix(currentPath);
-    currentSlug = currentPath === '/' ? 'home' : currentPath.replace('/', '').toLowerCase();
+    let currentPath = window.location.pathname;
+    currentPath = getPathAfterHelix(currentPath);
+    const currentSlug = currentPath === '/' ? 'home' : currentPath.replace('/', '').toLowerCase();
     // Find the menu item with the matching slug and add the 'active' class
     const menuItems = document.querySelectorAll('.nav-menu a');
     menuItems.forEach(item => {
